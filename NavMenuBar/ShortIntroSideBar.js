06fab6377dca16a65ff8b40e317462a3ab1ef5f3
Bende Lilla <bendelilla93@gmail.com>
2018-12-21 12:16:49 +0100

    Styling and fixing a bug in responsive menu
    -arrow placed before the menu in one line
    -responsive menu works with phones  too

--- NavMenuBar/ShortIntroSideBar.js
+++ NavMenuBar/ShortIntroSideBar.js
@@ -1,9 +1,9 @@
 function OpenSideBar() {
   document.getElementById("i_ShortIntroSideBar").style.width = "250px";
-  document.getElementById("i_MainContent").style.marginLeft = "250px";
+
 }
 
 function CloseSideBar() {
   document.getElementById("i_ShortIntroSideBar").style.width = "0";
-  document.getElementById("i_MainContent").style.marginLeft= "0";
+
 }
